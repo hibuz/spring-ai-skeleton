02f200a45d4862ee3fd67c9b04f3312980734c81
Hibuz <38822671+hibuz@users.noreply.github.com>
2024-11-22 07:10:29 +0000

    ✨ feat : support codespace swagger

--- src/main/java/com/hibuz/ai/config/SpringdocConfig.java
+++ src/main/java/com/hibuz/ai/config/SpringdocConfig.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +16,7 @@ public class SpringdocConfig {
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .components(new Components())
+                .addServersItem(new Server().url("/"))
                 .info(apiInfo());
     }
 
