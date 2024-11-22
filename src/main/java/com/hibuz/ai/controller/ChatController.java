008dd0125bf694e44e01159309c6ae25b3028f1d
hibuz <hibuz@hibuz.com>
2024-11-22 03:10:45 +0000

    ✨ feat : support ollama chat

--- src/main/java/com/hibuz/ai/controller/ChatController.java
+++ src/main/java/com/hibuz/ai/controller/ChatController.java
@@ -2,6 +2,8 @@ package com.hibuz.ai.controller;
 
 import java.util.Map;
 
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +14,16 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ChatController {
 
+    private final OllamaChatModel chatModel;
+
+    @Autowired
+    public ChatController(OllamaChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
     @GetMapping("/ai/generate")
     public Map<String, String> generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
         log.info("chat message={}", message);
-        return Map.of("generation", "hahaha");
+        return Map.of("generation", this.chatModel.call(message));
     }
 }
