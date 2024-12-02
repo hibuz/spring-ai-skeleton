e58e07fe09b25b88f507962e18e1930990d27940
hibuz <hibuz@hibuz.com>
2024-12-02 23:48:22 +0900

    🐛 fix : test by disabling dev profile

--- src/test/java/com/hibuz/ai/AiApplicationTests.java
+++ src/test/java/com/hibuz/ai/AiApplicationTests.java
@@ -2,8 +2,10 @@ package com.hibuz.ai;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class AiApplicationTests {
 
 	@Test
