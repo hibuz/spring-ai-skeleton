0a03f57fa61142ffdd03643b3646580d07806ab1
Hibuz <38822671+hibuz@users.noreply.github.com>
2024-12-05 13:25:20 +0000

    ✨ feat : support func callback

--- src/main/java/com/hibuz/ai/controller/FunctionController.java
+++ src/main/java/com/hibuz/ai/controller/FunctionController.java
@@ -6,16 +6,21 @@ import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.model.function.FunctionCallback;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hibuz.ai.service.WeatherService;
+import com.hibuz.ai.service.WeatherService.Unit;
 
 import lombok.extern.slf4j.Slf4j;
 
+@RequestMapping("func")
 @RestController
 @Slf4j
 public class FunctionController {
+
+    private static final String DEFAULT_VALUE = "What's the weather like in San Francisco, Seoul, and Paris?";
 
     private final ChatModel chatModel;
 
@@ -24,23 +29,39 @@ public class FunctionController {
         this.chatModel = chatModel;
     }
 
-    @GetMapping("func/bean")
-    public ChatResponse bean(@RequestParam(value = "message", defaultValue = "What's the weather like in San Francisco, Seoul, and Paris?") String message) {
+    @GetMapping("bean")
+    public ChatResponse bean(@RequestParam(value = "message", defaultValue = DEFAULT_VALUE) String message) {
         log.info("chat> {}", message);
+
         return ChatClient.create(chatModel).prompt(message)
             .functions("currentWeather")
             .call().chatResponse();
     }
 
-    @GetMapping("func/invoke")
-    public ChatResponse client(@RequestParam(value = "message", defaultValue = "What's the weather like in San Francisco, Seoul, and Paris?") String message) {
+    @GetMapping("callback/function-invoke")
+    public ChatResponse func(@RequestParam(value = "message", defaultValue = DEFAULT_VALUE) String message) {
         log.info("chat> {}", message);
-        return ChatClient.create(chatModel).prompt(message)
-            .functions(FunctionCallback.builder()
-            .description("Get the weather in location")
-            .function("currentWeather", (WeatherService.Request request) -> new WeatherService.Response(20.0, WeatherService.TemperatureUnit.FAHRENHEIT))
+        FunctionCallback callback = FunctionCallback.builder()
+            .function("currentWeather", (WeatherService.Request request) ->
+                 new WeatherService.Response(20.0, Unit.FAHRENHEIT))
             .inputType(WeatherService.Request.class)
-            .build())
+            .build();
+
+        return ChatClient.create(chatModel).prompt(message)
+            .functions(callback)
+            .call().chatResponse();
+    }
+
+    @GetMapping("callback/static-method-invoke")
+    public ChatResponse method(@RequestParam(value = "message", defaultValue = DEFAULT_VALUE + " Use Celsius.") String message) {
+        log.info("chat> {}", message);
+        FunctionCallback callback = FunctionCallback.builder()
+            .method("getWeatherStatic", String.class, Unit.class)
+            .targetClass(WeatherService.class)
+            .build();
+
+        return ChatClient.create(chatModel).prompt(message)
+            .functions(callback)
             .call().chatResponse();
     }
 }
--- src/main/java/com/hibuz/ai/service/WeatherService.java
+++ src/main/java/com/hibuz/ai/service/WeatherService.java
@@ -8,15 +8,32 @@ import org.springframework.stereotype.Component;
 @Component("currentWeather")
 @Description("Get the weather in location")
 public class WeatherService implements Function<WeatherService.Request, WeatherService.Response> {
+	
+    public enum Unit { CELSIUS, FAHRENHEIT }
 
-    public enum TemperatureUnit { CELSIUS, FAHRENHEIT }
+	public record Request(String location, Unit unit) {}
 
-	public record Request(String location, TemperatureUnit unit) {}
-
-	public record Response(double temp, TemperatureUnit unit) {}
+	public record Response(double temp, Unit unit) {}
 
     @Override
 	public Response apply(Request request) {
-		return new Response(30.0, TemperatureUnit.CELSIUS);
+		return new Response(30.0, Unit.CELSIUS);
+	}
+
+	@Description("Get weather information for a city")
+	public static String getWeatherStatic(String city, Unit unit) {
+
+		double temperature = 0;
+		if (city.contains("Paris")) {
+			temperature = 30;
+		}
+		else if (city.contains("Seoul")) {
+			temperature = 20;
+		}
+		else if (city.contains("San Francisco")) {
+			temperature = 10;
+		}
+
+		return "temperature: " + temperature + " unit: " + unit;
 	}
 }
