e786de8b28265424b3853c0fd68825682b75182e
hibuz <hibuz@hibuz.com>
2025-01-03 06:44:52 +0900

    ♻️ refactor : use OpenAPIDefinition annotation

--- src/main/java/com/hibuz/ai/config/SpringdocConfig.java
+++ src/main/java/com/hibuz/ai/config/SpringdocConfig.java
@@ -1,9 +1,8 @@
 package com.hibuz.ai.config;
 
-import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(info = @Info(title = "APIs", version = "0.0.1", description = "Spring AI skeleton project API"))
 public class SpringdocConfig {
 
     @Value("#{servletContext.contextPath}")
@@ -18,17 +18,6 @@ public class SpringdocConfig {
 
     @Bean
     public OpenAPI openAPI() {
-        return new OpenAPI()
-                .components(new Components())
-                .addServersItem(new Server().url(contextPath))
-                .info(apiInfo());
-    }
-
-    private Info apiInfo() {
-        return new Info()
-                .title("APIs")
-                .description("Spring AI skeleton project API")
-                .version("0.0.1")
-                .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0"));
+        return new OpenAPI().addServersItem(new Server().url(contextPath));
     }
 }
