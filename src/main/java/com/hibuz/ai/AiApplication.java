d06ef46a58ad4924f40bf45e305dd1810c0b49b7
hibuz <hibuz@hibuz.com>
2025-02-16 14:48:49 +0000

    ✨ feat : add actuator info, env api

--- src/main/java/com/hibuz/ai/AiApplication.java
+++ src/main/java/com/hibuz/ai/AiApplication.java
@@ -1,13 +1,22 @@
 package com.hibuz.ai;
 
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class AiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AiApplication.class, args);
 	}
-
+	@Bean
+    public ApplicationRunner run(ChatModel model) {
+        return args -> log.info("ChatModel: {}", model.getDefaultOptions().getModel());
+	}
 }
--- src/main/java/com/hibuz/ai/controller/ChatController.java
+++ src/main/java/com/hibuz/ai/controller/ChatController.java
@@ -1,8 +1,11 @@
 package com.hibuz.ai.controller;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.boot.actuate.info.Info.Builder;
+import org.springframework.boot.actuate.info.InfoContributor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
-public class ChatController {
+public class ChatController implements InfoContributor {
 
     private final OllamaChatModel chatModel;
 
@@ -19,9 +22,21 @@ public class ChatController {
         this.chatModel = chatModel;
     }
 
+    @GetMapping("/info")
+    public Map<String, Object> info() {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("ChatModel", this.chatModel);
+        return map;
+    }
+
     @GetMapping("/generate")
     public Map<String, String> generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
         log.info("chat message={}", message);
         return Map.of("generation", this.chatModel.call(message));
+    }
+
+    @Override
+    public void contribute(Builder builder) {
+        builder.withDetails(info());
     }
 }
