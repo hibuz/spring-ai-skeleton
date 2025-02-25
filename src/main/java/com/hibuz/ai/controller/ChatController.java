53717e07250be6905af76f74f8062548216a47bf
hibuz <hibuz@hibuz.com>
2025-02-25 11:03:05 +0000

    ✨ feat : add translation in korean lang

--- src/main/java/com/hibuz/ai/controller/ChatController.java
+++ src/main/java/com/hibuz/ai/controller/ChatController.java
@@ -1,15 +1,11 @@
 package com.hibuz.ai.controller;
 
-import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.ai.chat.messages.Message;
-import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.chat.prompt.PromptTemplate;
-import org.springframework.beans.factory.annotation.Value;
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.actuate.info.Info.Builder;
 import org.springframework.boot.actuate.info.InfoContributor;
-import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,8 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.hibuz.ai.service.ChatClientService;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,9 +22,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Slf4j
 @Tag(name = "step1", description = "Chat Client API")
 public class ChatController implements InfoContributor {
-
-	@Value("classpath:/prompts/qna-korean-only.st")
-	private Resource qnaPrompt;
 
     private final ChatClientService service;
 
@@ -48,31 +39,17 @@ public class ChatController implements InfoContributor {
          return service.getModelInfo();
     }
 
-    @PostMapping("/model/{model}/{optionModel}")
-    @Operation(summary = "model: ollama(llama3.2:1b,3b, deepseek-r1:1.5b,7b,8b,14b, llama3.2-vision:11b, llava:7b,13b, llava-llama3:8b)")
-    @Parameter(name = "model", example = "ollama")
-    @Parameter(name = "optionModel", example = "llama3.2:3b")
-    public Map<String, Object> changeModel(@PathVariable String model, @PathVariable String optionModel) {
-        service.changeChatModel(model, optionModel);
+    @PostMapping("/model/{modelTypeName}/{modelName}")
+    public Map<String, Object> changeModel(@Schema(example = "ollama") @PathVariable String modelTypeName,
+                                           @Schema(example = "ollama(llama3.2:1b,3b, qwen2.5:0.5b,1.5b,3b,7b, deepseek-r1:1.5b,7b,8b,14b, phi4:7b, mistral14b, llama3.2-vision:11b, llava:7b,13b, llava-llama3:8b)")
+                                           @PathVariable String modelName) {
+        service.changeChatModel(StringUtils.trim(modelTypeName), StringUtils.trim(modelName));
         return info();
     }
 
     @GetMapping("/generate")
-    public Map<String, String> generate(@RequestParam(defaultValue = "Tell me a joke") String message, @RequestParam(defaultValue = "true") boolean korean) {
+    public String generate(@RequestParam(defaultValue = "Tell me a joke") String message) {
         log.info("chat> {}", message);
-
-        Prompt prompt;
-        if (korean) {
-            PromptTemplate template = new PromptTemplate(qnaPrompt);
-            Message promptMessage = template.createMessage(Map.of("question", message));
-            prompt = new Prompt(promptMessage);
-        } else {
-            prompt = new Prompt(message);
-        }
-
-        Map<String, String> result = new HashMap<>();
-        result.put("generation", service.chat(prompt));
-
-        return result;
+        return service.chat(message);
     }
 }
--- src/main/java/com/hibuz/ai/controller/PromptController.java
+++ src/main/java/com/hibuz/ai/controller/PromptController.java
@@ -1,8 +1,11 @@
 package com.hibuz.ai.controller;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.prompt.Prompt;
@@ -10,6 +13,7 @@ import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.chat.prompt.SystemPromptTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,6 +34,9 @@ public class PromptController {
     @Value("classpath:/prompts/role-prompt.st")
 	private Resource rolePrompt;
 
+    @Value("classpath:/prompts/qna-korean-prompt.st")
+    private Resource qnaKoreanPrompt;
+
 	@Value("classpath:/prompts/qna-prompt.st")
 	private Resource qnaPrompt;
 
@@ -43,7 +50,7 @@ public class PromptController {
     }
 
     @GetMapping("/template")
-	@Operation(summary = "system prompt template : Tell me a {adjective} joke about {topic}")
+	@Operation(summary = "1", description = "system prompt template : Tell me a {adjective} joke about {topic}", externalDocs = @ExternalDocumentation(url = "https://news.hada.io/topic?id=13379"))
     public String template(@RequestParam(defaultValue = "funny") String adjective, @RequestParam(defaultValue = "cows") String topic) {
 
         log.info("chat> Tell me a {} joke about {}", adjective, topic);
@@ -54,8 +61,11 @@ public class PromptController {
     }
 
     @GetMapping("/roles")
-	public String roles(@RequestParam(
-			defaultValue = "Tell me about three famous pirates from the Golden Age of Piracy and why they did.  Write at least a sentence for each pirate.") String message,
+    @Operation(summary = "2")
+	public String roles(@Schema(description = "해적의 황금시대의 유명한 해적 3명과 그들이 왜 그랬는지 말해주세요. 각 해적에 대해 최소한 한 문장을 쓰세요.",
+                                defaultValue = "Tell me about three famous pirates from the Golden Age of Piracy and why they did." +
+                                    "Write at least a sentence for each pirate.")
+            @RequestParam String message,
 			@RequestParam(defaultValue = "Bob") String name,
 			@RequestParam(defaultValue = "pirate") String voice) {
 
@@ -68,7 +78,40 @@ public class PromptController {
         return service.chat(prompt);
 	}
 
+    @GetMapping("/korean")
+    @Operation(summary = "3")
+    public Map<String, String> korean(@RequestParam(defaultValue = "What is the most important reason to learn AI in one sentence?") String message,
+                                      @Schema(example = "If the following sentence contains a lot of English words, " +
+                                              "translate it into very natural Korean.")
+                                      @RequestParam(required = false) String translateTemplate,
+                                      @RequestParam(defaultValue = "false") boolean useQnaPromptForKorean) {
+
+        log.info("chat> {}", message);
+        Prompt prompt;
+        if (useQnaPromptForKorean) {
+            PromptTemplate template = new PromptTemplate(qnaKoreanPrompt);
+            Message promptMessage = template.createMessage(Map.of("question", message));
+            prompt = new Prompt(promptMessage);
+        } else {
+            prompt = new Prompt(message);
+        }
+
+        String response = service.chat(prompt);
+
+        Map<String, String> result = new HashMap<>();
+        result.put("generation", response);
+        if (StringUtils.hasText(translateTemplate)) {
+            PromptTemplate promptTemplate =
+                    new PromptTemplate(translateTemplate + "\n\n{sentence}", Map.of("sentence", response));
+            String translated = service.chat(promptTemplate.create());
+            result.put("translated", translated);
+        }
+
+        return result;
+    }
+
     @GetMapping("/stuff")
+    @Operation(summary = "4")
 	public String stuff(@RequestParam(defaultValue = "Which athletes won the mixed doubles gold medal in curling at the 2022 Winter Olympics?'") String message,
 			@RequestParam(defaultValue = "false") boolean stuffit) {
 
