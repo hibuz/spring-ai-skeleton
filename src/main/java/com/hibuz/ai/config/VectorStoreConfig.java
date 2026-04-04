b8a91c0045f08d595040e4ad865de503cfe6b6ea
hibuz <hibuz@hibuz.com>
2026-04-04 09:06:49 +0900

    feat: update spring ai to 2.0.0-M4 and boot to 4.0.5

--- src/main/java/com/hibuz/ai/config/VectorStoreConfig.java
+++ src/main/java/com/hibuz/ai/config/VectorStoreConfig.java
@@ -5,18 +5,20 @@ import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.ai.vectorstore.pgvector.PgVectorStore.PgVectorStoreBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+@Configuration
 public class VectorStoreConfig {
 
-    //@Profile({ "dev", "prod" })
-    //@Bean
+    @Profile({ "dev", "prod" })
+    @Bean
     public VectorStore pgVectorStore(PgVectorStoreBuilder builder) {
         return builder.build();
     }
 
-    //@Profile("!dev & !prod")
-    //@Bean
+    @Profile("!dev & !prod")
+    @Bean
     public VectorStore simplVectorStore(EmbeddingModel embeddingModel) {
         return SimpleVectorStore.builder(embeddingModel).build();
     }
